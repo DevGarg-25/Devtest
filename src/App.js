8ad1cc80517479805455e091825b5c73310f1290
Dev Garg <tecnospark.power2@gmail.com>
2023-06-17 19:27:46 +0530

    React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import './App.css';
+export default function App() {
+  // ...
 
-function App() {
+
+const AdmissionForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, phone, address);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Admission Form</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input type="text" name="name" value={name} placeholder="Enter your Name" onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" value={email} placeholder="Enter your Email" onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <label>Phone:</label>
+          <input type="text" name="phone" value={phone} placeholder="Enter your Contact Number" onChange={(e) => setPhone(e.target.value)} />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input type="text" name="address" value={address} placeholder="Enter your Address" onChange={(e) => setAddress(e.target.value)} />
+          Click to Submit
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-}
+};
 
-export default App;
+ReactDOM.render(<AdmissionForm />, document.getElementById("root"));
+}
